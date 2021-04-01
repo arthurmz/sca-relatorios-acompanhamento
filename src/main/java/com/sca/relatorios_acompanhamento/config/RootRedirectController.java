5e0a872351ff18bf07edd1fc52d2ffe6a81e8a32
Arthur <jamacedo1@stefanini.com>
2021-03-31 21:27:24 -0300

    Versão mais recente

--- src/main/java/com/sca/relatorios_acompanhamento/config/RootRedirectController.java
+++ src/main/java/com/sca/relatorios_acompanhamento/config/RootRedirectController.java
@@ -1,11 +1,13 @@
-package com.sca.relatorios_acompanhamento;
+package com.sca.relatorios_acompanhamento.config;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Controller
 @RequestMapping("/")
+@ApiIgnore
 public class RootRedirectController {
     @GetMapping
     public String swaggerUi() {
